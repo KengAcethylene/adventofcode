43a8b954ebe0e901d6198618b35bc1fdc438ef01
Chanchai Rattanasiwakoon <keng2120@gmail.com>
2023-12-09 13:04:51 +0700

    add: day 9 p1 & 2

---
+++ day9/p2.js
@@ -0,0 +1,30 @@
+const { readFileSync } = require('fs');
+
+const lines = readFileSync(`${__dirname}/text.txt`, 'utf-8')
+    .split('\n')
+    .map((line) => line.split(' ').map((x) => parseInt(x)));
+
+for (const line of lines) {
+    line.reverse();
+}
+
+function process(line) {
+    if (!line.every((num) => num === line[0])) {
+        const newLine = line.reduce((prev, curr, idx) => {
+            if (prev.length !== 0) {
+                prev[prev.length - 1] = curr - prev[prev.length - 1];
+            }
+            if (idx !== line.length - 1) {
+                prev.push(curr);
+            }
+            return prev;
+        }, []);
+
+        return line[line.length - 1] + process(newLine);
+    } else {
+        return line[0];
+    }
+}
+
+const sum = lines.reduce((prev, line) => prev + process(line), 0);
+console.log(sum);
