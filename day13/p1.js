0c3c99243006f1548187cb5657230fa8f52c230f
Chanchai Rattanasiwakoon <keng2120@gmail.com>
2023-12-13 21:06:15 +0700

    update: improve day13

--- day13/p1.js
+++ day13/p1.js
@@ -1,36 +1,33 @@
 const { readFileSync } = require('fs');
 
-const rows = readFileSync(`${__dirname}/text.txt`, 'utf-8').split('\n');
-let pattern = [];
-let sum = 0;
-for (const row of rows){
-    if (row !== ''){
-        pattern.push(row)
-    }else {
-        let transposePattern = pattern[0].split('').map((_, colIndex) => pattern.map(row => row[colIndex])).map(row => row.join(""));
-        const note = getMiddle(pattern)*100 + getMiddle(transposePattern);
-        sum += note;
-        pattern = []
-    }
-}
+const rows = readFileSync(`${__dirname}/text.txt`, 'utf-8').split('\n\n');
+
+const sum = rows.reduce((prev, curr) => {
+    const pattern = curr.split('\n');
+    const transposePattern = pattern[0]
+        .split('')
+        .map((_, colIndex) => pattern.map((row) => row[colIndex]))
+        .map((row) => row.join(''));
+    return prev + getMiddle(pattern) * 100 + getMiddle(transposePattern);
+}, 0);
 
 console.log(sum);
 
-function getMiddle(patterns){
-    for (let row = 0; row < patterns.length-1;row++){
+function getMiddle(patterns) {
+    for (let row = 0; row < patterns.length - 1; row++) {
         let left = row;
-        let right = row+1;
+        let right = row + 1;
         let isMirror = true;
-        while(left >= 0 && right < patterns.length){
-            if (patterns[left] !== patterns[right]){
+        while (left >= 0 && right < patterns.length) {
+            if (patterns[left] !== patterns[right]) {
                 isMirror = false;
                 break;
             }
             left--;
             right++;
         }
-        if (isMirror){
-            return row+1;
+        if (isMirror) {
+            return row + 1;
         }
     }
 
--- day13/p2.js
+++ day13/p2.js
@@ -1,37 +1,34 @@
 const { readFileSync } = require('fs');
 
-const rows = readFileSync(`${__dirname}/text.txt`, 'utf-8').split('\n');
-let pattern = [];
-let sum = 0;
-for (const row of rows){
-    if (row !== ''){
-        pattern.push(row)
-    }else {
-        let transposePattern = pattern[0].split('').map((_, colIndex) => pattern.map(row => row[colIndex])).map(row => row.join(""));
-        const note = getMiddle(pattern)*100 + getMiddle(transposePattern);
-        sum += note;
-        pattern = []
-    }
-}
+const rows = readFileSync(`${__dirname}/text.txt`, 'utf-8').split('\n\n');
+
+const sum = rows.reduce((prev, curr) => {
+    const pattern = curr.split('\n');
+    const transposePattern = pattern[0]
+        .split('')
+        .map((_, colIndex) => pattern.map((row) => row[colIndex]))
+        .map((row) => row.join(''));
+    return prev + getMiddle(pattern) * 100 + getMiddle(transposePattern);
+}, 0);
 
 console.log(sum);
 
-function getMiddle(patterns){
-    for (let row = 0; row < patterns.length-1;row++){
+function getMiddle(patterns) {
+    for (let row = 0; row < patterns.length - 1; row++) {
         let left = row;
-        let right = row+1;
+        let right = row + 1;
         let isMirror = true;
         let isSmudged = false;
-        while(left >= 0 && right < patterns.length){
-            if (patterns[left] !== patterns[right]){
-                if (isSmudged){
+        while (left >= 0 && right < patterns.length) {
+            if (patterns[left] !== patterns[right]) {
+                if (isSmudged) {
                     isMirror = false;
                     break;
-                }else{
-                    if(checkSmudge(patterns[left], patterns[right]) ){
+                } else {
+                    if (checkSmudge(patterns[left], patterns[right])) {
                         isSmudged = true;
-                    }else{
-                        isMirror= false;
+                    } else {
+                        isMirror = false;
                         break;
                     }
                 }
@@ -39,20 +36,21 @@ function getMiddle(patterns){
             left--;
             right++;
         }
-        if (isMirror && isSmudged){
-            return row+1;
+        if (isMirror && isSmudged) {
+            return row + 1;
         }
     }
 
     return 0;
 }
 
-function checkSmudge(left, right){
-    let diff = 0;
-    for (let i=0; i<left.length; i++){
-        if(left[i] !== right[i]){
-            diff++;
-        }
-    }
-    return diff === 1;
+function checkSmudge(left, right) {
+    return (
+        left
+            .split('')
+            .reduce(
+                (prev, curr, idx) => (curr !== right[idx] ? prev + 1 : prev),
+                0
+            ) === 1
+    );
 }
