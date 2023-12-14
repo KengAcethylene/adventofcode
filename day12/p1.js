ed60c4a0eb40c1e0c616b12f22eb9b354ea9f14b
kengacethylene <keng2120@gmail.com>
2023-12-14 16:49:02 +0700

    update: lint on day12 & day14

--- day12/p1.js
+++ day12/p1.js
@@ -2,10 +2,10 @@ const { readFileSync } = require('fs');
 
 const rows = readFileSync(`${__dirname}/text.txt`, 'utf-8').split('\n');
 
-let total = 0
+let total = 0;
 for (const row of rows) {
     let [cfg, nums] = row.split(' ');
-    nums = nums.split(",").map((n) => parseInt(n));
+    nums = nums.split(',').map((n) => parseInt(n));
     const count = process(cfg, nums);
     console.log(cfg, nums, count);
     total += count;
@@ -24,15 +24,18 @@ function process(cfg, nums) {
 
     let result = 0;
 
-    if (".?".includes(cfg[0])) {
+    if ('.?'.includes(cfg[0])) {
         result += process(cfg.slice(1), nums);
     }
 
-    if ("#?".includes(cfg[0])) {
-        if (nums[0] <= cfg.length && !cfg.slice(0, nums[0]).includes('.') && (cfg.length === nums[0] || cfg[nums[0]] !== '#')) {
-            result += process(cfg.slice(nums[0] + 1), nums.slice(1))
-        }
-        else {
+    if ('#?'.includes(cfg[0])) {
+        if (
+            nums[0] <= cfg.length &&
+            !cfg.slice(0, nums[0]).includes('.') &&
+            (cfg.length === nums[0] || cfg[nums[0]] !== '#')
+        ) {
+            result += process(cfg.slice(nums[0] + 1), nums.slice(1));
+        } else {
             result += 0;
         }
     }
--- day12/p2.js
+++ day12/p2.js
@@ -2,13 +2,17 @@ const { readFileSync } = require('fs');
 
 const rows = readFileSync(`${__dirname}/text.txt`, 'utf-8').split('\n');
 
-const cache = {}
+const cache = {};
 
-let total = 0
+let total = 0;
 for (const row of rows) {
     let [cfg, nums] = row.split(' ');
     cfg = new Array(5).fill(cfg).join('?');
-    nums = new Array(5).fill(nums).join(',').split(',').map((x) => parseInt(x));
+    nums = new Array(5)
+        .fill(nums)
+        .join(',')
+        .split(',')
+        .map((x) => parseInt(x));
     const count = process(cfg, nums);
     total += count;
 }
@@ -24,22 +28,25 @@ function process(cfg, nums) {
         return cfg.includes('#') ? 0 : 1;
     }
 
-    let key = `${cfg}${nums.toString()}`
-    if (cache[key] !== undefined){
+    let key = `${cfg}${nums.toString()}`;
+    if (cache[key] !== undefined) {
         return cache[key];
     }
 
     let result = 0;
 
-    if (".?".includes(cfg[0])) {
+    if ('.?'.includes(cfg[0])) {
         result += process(cfg.slice(1), nums);
     }
 
-    if ("#?".includes(cfg[0])) {
-        if (nums[0] <= cfg.length && !cfg.slice(0, nums[0]).includes('.') && (cfg.length === nums[0] || cfg[nums[0]] !== '#')) {
-            result += process(cfg.slice(nums[0] + 1), nums.slice(1))
-        }
-        else {
+    if ('#?'.includes(cfg[0])) {
+        if (
+            nums[0] <= cfg.length &&
+            !cfg.slice(0, nums[0]).includes('.') &&
+            (cfg.length === nums[0] || cfg[nums[0]] !== '#')
+        ) {
+            result += process(cfg.slice(nums[0] + 1), nums.slice(1));
+        } else {
             result += 0;
         }
     }
