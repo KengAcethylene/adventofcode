6c7a681f9360b70a7344285255c8186938e7249d
Chanchai Rattanasiwakoon <keng2120@gmail.com>
2023-12-16 21:09:54 +0700

    add: day 16 p1 & 2

---
+++ day16/p1.js
@@ -0,0 +1,67 @@
+const { readFileSync } = require('fs');
+
+const rows = readFileSync(`${__dirname}/text.txt`, 'utf-8')
+    .split('\n')
+    .map((x) => x.split(''));
+
+const next = [[0, 0, 0, 1]];
+const seen = new Set();
+
+while (next.length !== 0) {
+    const [r, c, rOff, cOff] = next.pop();
+    if (r < 0 || r >= rows.length || c < 0 || c >= rows[0].length) {
+        continue;
+    }
+    if (seen.has(JSON.stringify([r, c, rOff, cOff]))) {
+        continue;
+    }
+
+    const char = rows[r][c];
+    if (char === '.') {
+        next.push([r + rOff, c + cOff, rOff, cOff]);
+    }
+
+    if (char === '/' || char === '\\') {
+        const mirror = char === '/' ? -1 : 1;
+        if (rOff) {
+            next.push([r, c + rOff * mirror, 0, rOff * mirror]);
+        }
+
+        if (cOff) {
+            next.push([r + cOff * mirror, c, cOff * mirror, 0]);
+        }
+    }
+
+    if (char === '|') {
+        if (rOff) {
+            next.push([r + rOff, c + cOff, rOff, cOff]);
+        }
+
+        if (cOff) {
+            for (const mirror of [-1, 1]) {
+                next.push([r + mirror, c, mirror, 0]);
+            }
+        }
+    }
+
+    if (char === '-') {
+        if (rOff) {
+            for (const mirror of [-1, 1]) {
+                next.push([r, c + mirror, 0, mirror]);
+            }
+        }
+
+        if (cOff) {
+            next.push([r + rOff, c + cOff, rOff, cOff]);
+        }
+    }
+    seen.add(JSON.stringify([r, c, rOff, cOff]));
+}
+
+const coordinate = [...seen].reduce((prev, curr) => {
+    const [r, c, ,] = JSON.parse(curr);
+    prev.add(JSON.stringify([r, c]));
+    return prev;
+}, new Set());
+
+console.log(coordinate.size);
